158e06b96245e50bd9ce83729d17cbed00671b31
Ehsaan Mondal <mail.ehsaan@gmail.com>
2025-12-09 18:42:44 +0530

    test pageImageSEO

--- src/utils/SeoArray.jsx
+++ src/utils/SeoArray.jsx
@@ -7,7 +7,7 @@ export const seoArray = [
         page_name: "page name /",
         page_title: "Kuick Studio",
         page_description: "/ desc",
-        page_image: "",
+        page_image: "https://images.unsplash.com/photo-1765154017463-5ca216ec2b67?q=80&w=1175&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         page_keyword: "/ keyword.",
     },
     {
@@ -18,7 +18,7 @@ export const seoArray = [
         page_name: "page name Home",
         page_title: "Kuick Studio",
         page_description: "Home desc",
-        page_image: "",
+        page_image: "https://images.unsplash.com/photo-1765084217276-270308cf62cb?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         page_keyword: "home keyword.",
     },
     {
@@ -29,7 +29,7 @@ export const seoArray = [
         page_name: "About",
         page_title: "About",
         page_description: "About desc",
-        page_image: "",
+        page_image: "https://images.unsplash.com/photo-1765154017463-5ca216ec2b67?q=80&w=1175&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         page_keyword: "About keyword.",
     },
 
